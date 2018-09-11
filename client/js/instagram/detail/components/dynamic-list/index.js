7f111b819b5bcca84f7a8cb6d6c7e635423cc123
sushi <codingzx@gmail.com>
2018-09-11 20:07:22 +0800

    update: 修改首页样式

--- client/js/instagram/detail/components/dynamic-list/index.js
+++ client/js/instagram/detail/components/dynamic-list/index.js
@@ -4,6 +4,17 @@ import Style from './index.scss'
 class DynamicList extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      friend_list: [
+        {
+          userInfo: {
+            avatar: 'https://scontent-nrt1-1.cdninstagram.com/vp/411304988011e5a322e837c6d44b5b35/5C2DA515/t51.2885-19/s320x320/33885471_1992324384411933_7383797600782123008_n.jpg',
+            username: 'codingzx',
+            abstract: 'life is great'
+          }
+        }
+      ],
+    }
   }
 
   render() {
