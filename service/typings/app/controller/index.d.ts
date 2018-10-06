057ab1190e8c17b62757e4b69f074be5753dd5a8
sushi <codingzx@gmail.com>
2018-10-06 12:33:57 +0800

    feat: add service

---
+++ service/typings/app/controller/index.d.ts
@@ -0,0 +1,11 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import 'egg'; // Make sure ts to import egg declaration at first
+import Home from '../../../app/controller/home';
+
+declare module 'egg' {
+  interface IController {
+    home: Home;
+  }
+}
---
+++ service/typings/config/index.d.ts
@@ -0,0 +1,15 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import 'egg'; // Make sure ts to import egg declaration at first
+import { EggAppConfig } from 'egg';
+import ExportConfigDefault from '../../config/config.default';
+import ExportConfigLocal from '../../config/config.local';
+import ExportConfigProd from '../../config/config.prod';
+type ConfigDefault = ReturnType<typeof ExportConfigDefault>;
+type ConfigLocal = ReturnType<typeof ExportConfigLocal>;
+type ConfigProd = ReturnType<typeof ExportConfigProd>;
+declare module 'egg' {
+  type NewEggAppConfig = ConfigDefault & ConfigLocal & ConfigProd;
+  interface EggAppConfig extends NewEggAppConfig { }
+}
